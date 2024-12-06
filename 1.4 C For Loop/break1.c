fca0aa6530014fd74ee900f71153a1e93ac80ef9
triettapcode <minhtriett463@gmail.com>
2024-12-06 22:30:23 +0700

    C

---
+++ 1.4 C For Loop/break1.c
@@ -0,0 +1,20 @@
+// Program to calculate the sum of numbers (10 numbers max)
+// If the user enters a negative number , the loop terminates
+#include <stdio.h> 
+int main () {
+	int i;
+	double number , sum = 0.0;
+	for (i = 1; i <= 10; ++i){
+		printf("Enter n%d: ", i);
+		scanf("%lf", &number);
+		// if the user enters a positive number, break the loop
+		if (number < 0.0) {
+			break;
+		}
+		sum += number; // sum = sum +number;
+		
+	
+	}
+	printf("sum = %.2lf", sum);
+	return 0;
+}
